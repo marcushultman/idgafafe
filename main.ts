7ae10ac2e96c28f012561a5e72cccd727c591291
Marcus Hultman <marcus.hultman1@gmail.com>
2024-03-09 20:29:22 -0500

    initial implementation

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { serveDir, ServeDirOptions } from 'https://deno.land/std@0.219.1/http/file_server.ts';
+
+const ENCODE = new TextEncoder();
+
+export async function handler(req: Request, opts?: ServeDirOptions) {
+  const { pathname } = new URL(req.url);
+  const res = await serveDir(req, opts);
+
+  if (res.ok && res.body && pathname.endsWith('.d.ts')) {
+    const text = await res.text();
+    const bytes = ENCODE.encode(text.replaceAll('.js', '.d.ts'));
+    res.headers.set('content-type', 'application/typescript');
+    res.headers.set('content-length', bytes.byteLength.toString());
+    return new Response(bytes, res);
+  }
+
+  if (res.ok && pathname.endsWith('.js')) {
+    const types = new URL(pathname.slice(0, -3) + '.d.ts', req.url).toString();
+    res.headers.set('x-typescript-types', types);
+  }
+
+  return res;
 }
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  Deno.serve((req) => handler(req));
 }
--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,22 @@
-import { assertEquals } from "https://deno.land/std@0.218.0/assert/mod.ts";
-import { add } from "./main.ts";
+import {
+  assert,
+  assertEquals,
+  assertExists,
+  assertFalse,
+} from 'https://deno.land/std@0.218.0/assert/mod.ts';
+import { handler } from './main.ts';
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test(async function typesHeaderTest() {
+  const res = await handler(new Request('http://dummy/data/index.js'), { quiet: true });
+  assertEquals(res.headers.get('x-typescript-types'), 'http://dummy/data/index.d.ts');
+  assertExists(res.body);
+  await res.body.cancel();
+});
+
+Deno.test(async function typesFileTest() {
+  const res = await handler(new Request('http://dummy/data/index.d.ts'), { quiet: true });
+  const text = await res.text();
+  assertFalse(res.headers.has('x-typescript-types'));
+  assertFalse(text.includes('foo.js'));
+  assert(text.includes('foo.d.ts'));
 });
